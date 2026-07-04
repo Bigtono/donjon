8b738d3ac9e091d836a99bcbf382dbe8d69662e7
Bigtono <jm-ml@bolls.net>
2026-07-04 12:47:04 +0200

    FIX db config local/ovh

---
+++ db.local.php
@@ -0,0 +1,15 @@
+<?php
+return [
+    'prod' => [
+        'user' => 'maikasteiymaika',
+        'pass' => 'Ovh150222250675',
+        'dsn'  => 'mysql:host=maikasteiymaika.mysql.db;dbname=maikasteiymaika',
+        'site' => 'maikastel.fr',
+    ],
+    'dev' => [
+        'user' => 'maikasteiymaika',
+        'pass' => 'Mai15022228',
+        'dsn'  => 'mysql:host=maikasteiymaika.mysql.db;dbname=maikasteiymaika',
+        'site' => 'maikastel.fr',
+    ],
+];
